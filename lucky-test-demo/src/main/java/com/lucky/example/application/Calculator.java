b5fedc7510b44af1c24d4bc47428add1722ec570
jianbao.huang <jianbao.huang@luckincoffee.com>
2020-09-17 09:29:56 +0800

    demo的Calculator新增2个方法

--- lucky-test-demo/src/main/java/com/lucky/example/application/Calculator.java
+++ lucky-test-demo/src/main/java/com/lucky/example/application/Calculator.java
@@ -8,4 +8,12 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int max(int a, int b) {
+		return a > b ? a : b;
+	}
 }
